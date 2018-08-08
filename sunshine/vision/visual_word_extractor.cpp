e976352f132ea6835f90e178237e5f0de23f8bfb
Levi Veevee Cai <levi@levicai.com>
2018-08-07 22:30:51 -0400

    Changed: added callback for pointclouds as depth maps for visual word extractors

--- sunshine/vision/visual_word_extractor.cpp
+++ sunshine/vision/visual_word_extractor.cpp
@@ -3,12 +3,17 @@
 #include "sensor_msgs/image_encodings.h"
 #include "cv_bridge/cv_bridge.h"
 
+#include "sensor_msgs/PointCloud2.h"
+#include "pcl_ros/point_cloud.h"
+#include "pcl/io/pcd_io.h"
+
 #include "visualwords/image_source.hpp"
 #include "visualwords/texton_words.hpp"
 #include "visualwords/feature_words.hpp"
 #include "visualwords/color_words.hpp"
 
 #include "sunshine_msgs/WordObservation.h"
+#include "geometry_msgs/TransformStamped.h"
 
 #include <iostream>
 #include <boost/filesystem.hpp>
@@ -19,25 +24,54 @@ namespace sunshine{
 
   static ros::Publisher words_pub;
   static MultiBOW multi_bow;
+  static geometry_msgs::TransformStamped latest_transform;
+  static bool transform_recvd; // TODO: smarter way of handling stale/missing poses
+  static bool depth_recvd;
+  static cv::Mat *depth_map;
+
+  void transformCallback(const geometry_msgs::TransformStampedPtr& msg){
+    if (!transform_recvd) transform_recvd = true;
+      
+    latest_transform = *msg;
+  }
+
+  void pcCallback(const sensor_msgs::PointCloud2ConstPtr& msg){
+    // TODO: probably a better way to do this, assumes pixel coordinates and non-overlapping z coordinates
+    // convert point cloud into a depth map
+    // Source: https://answers.ros.org/question/136916/conversion-from-sensor_msgspointcloud2-to-pclpointcloudt/
+
+    if (!depth_recvd){
+      depth_map = new cv::Mat(msg->height, msg->width, CV_64FC1);
+      depth_recvd = true;
+    }
+    
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*msg,pcl_pc2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_pcl(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2,*tmp_pcl);
+
+    for (auto pt : tmp_pcl->points){
+      depth_map->at<double>(cv::Point((int)pt.x,(int)pt.y)) = (double)pt.z;
+    }
+  }
   
   void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 
+    if (!transform_recvd){
+      ROS_WARN("No transformation received, transformations may be incorrect");
+      return;
+    }
+    
     cv_bridge::CvImageConstPtr img_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
     
     cv::Mat img = img_ptr->image;
 
-    //cv::imshow("Test",img);
-    //cv::waitKey(3);
-    
     WordObservation z = multi_bow(img);
     sunshine_msgs::WordObservation::Ptr sz(new sunshine_msgs::WordObservation);
 
-    geometry_msgs::TransformStamped transform;
-    transform.header.stamp = ros::Time::now();
-    
     sz->source = z.source;
     sz->seq = msg->header.seq;
-    sz->observation_transform = transform;
+    sz->observation_transform = latest_transform;
     sz->vocabulary_begin = z.vocabulary_begin;
     sz->vocabulary_size = z.vocabulary_size;
     sz->words = z.words;
@@ -63,9 +97,10 @@ int main(int argc, char** argv){
     data_root = data_root_c;
   }
 
-  std::string vocabulary_filename, texton_vocab_filename, image_topic_name, feature_descriptor_name;
+  std::string vocabulary_filename, texton_vocab_filename, image_topic_name,
+    feature_descriptor_name, pc_topic_name, transform_topic_name;
   int num_surf, num_orb, color_cell_size, texton_cell_size;
-  bool use_surf, use_hue, use_intensity, use_orb, use_texton;
+  bool use_surf, use_hue, use_intensity, use_orb, use_texton, use_pc;
   double img_scale;
 
   // Parse parameters
@@ -88,6 +123,10 @@ int main(int argc, char** argv){
   
   nhp.param<double>("scale", img_scale, 1.0);
   nhp.param<string>("image", image_topic_name, "/camera/image_raw");
+  nhp.param<string>("transform", transform_topic_name, "/camera_world_transform");
+  nhp.param<string>("pc", pc_topic_name, "/point_cloud");
+  nhp.param<bool>("use_pc", use_pc, false);
+  
   nhp.param<double>("rate", rate, 0);
 
   nhp.param<string>("feature_descriptor",feature_descriptor_name, "ORB");
@@ -99,6 +138,7 @@ int main(int argc, char** argv){
     feature_detector_names.push_back("SURF");
     feature_sizes.push_back(num_surf);
   }
+
   if(use_orb){
     feature_detector_names.push_back("ORB");
     feature_sizes.push_back(num_orb);
@@ -125,6 +165,9 @@ int main(int argc, char** argv){
   image_transport::Subscriber sub = it.subscribe(image_topic_name, 1, sunshine::imageCallback);
 
   sunshine::words_pub = nhp.advertise<sunshine_msgs::WordObservation>("words", 1);
+
+  sunshine::transform_recvd = false;
+  sunshine::depth_recvd = false;
 
   if(rate <= 0)
     ros::spin();
