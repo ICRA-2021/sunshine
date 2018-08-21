1a20f6dfe5f50745379baf655122c2d0689ccdae
Stewart Jamieson <sjamieson@whoi.edu>
2018-08-21 11:18:31 -0400

    Added: TopicMap message and implementation for a lower bandwidth way to transmit/visualize topic+ppx maps.

--- sunshine/topic_model/topic_model.hpp
+++ sunshine/topic_model/topic_model.hpp
@@ -5,9 +5,6 @@
 #include <memory>
 #include <ros/ros.h>
 #include <rost/rost.hpp>
-#include <sunshine_msgs/LocalSurprise.h>
-#include <sunshine_msgs/Perplexity.h>
-#include <sunshine_msgs/TopicWeights.h>
 #include <sunshine_msgs/WordObservation.h>
 
 #define POSEDIM 4
@@ -22,20 +19,22 @@ typedef neighbors<cell_pose_t> neighbors_t;
 typedef ROST<cell_pose_t, neighbors_t, hash_container<cell_pose_t>> ROST_t;
 
 class topic_model {
+    static std::vector<std::string> const VALID_MAP_PPX_TYPES;
     ros::NodeHandle* nh;
-    ros::Publisher scene_pub, global_perplexity_pub, global_surprise_pub, local_surprise_pub, topic_weights_pub;
+    ros::Publisher scene_pub, global_perplexity_pub, global_surprise_pub, local_surprise_pub, topic_weights_pub, map_pub;
     ros::Subscriber word_sub;
 
     std::mutex wordsReceivedLock;
     std::chrono::steady_clock::time_point lastWordsAdded;
-    int consecutive_rate_violations = 0;
+    mutable int consecutive_rate_violations = 0;
 
     std::string words_topic_name;
     int K, V, last_time; //number of topic types, number of word types
     double cell_size_time, cell_size_space, k_alpha, k_beta, k_gamma, k_tau, p_refine_rate_local, p_refine_rate_global;
     CellDimType G_time, G_space;
     int num_threads, min_obs_refine_time, obs_queue_size;
-    bool polled_refine, update_topic_model, publish_topics, publish_local_surprise, publish_global_surprise, publish_ppx;
+    bool polled_refine, update_topic_model, publish_topics, publish_local_surprise, publish_global_surprise, publish_ppx, publish_map;
+    std::string map_ppx_type;
     size_t last_refine_count;
     std::unique_ptr<ROST_t> rost;
 
@@ -46,9 +45,9 @@ class topic_model {
     std::atomic<bool> stopWork;
     std::vector<std::shared_ptr<std::thread>> workers;
 
-    void wait_for_processing();
+    void wait_for_processing() const;
     void words_callback(const sunshine_msgs::WordObservation::ConstPtr& words);
-    void broadcast_topics();
+    void broadcast_topics() const;
 
 public:
     topic_model(ros::NodeHandle* nh);
