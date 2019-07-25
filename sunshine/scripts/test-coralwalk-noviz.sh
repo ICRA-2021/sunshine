5e6bcf3c175ed4687085b72cfca29e7bbc079644
Stewart Jamieson <sjamieson@whoi.edu>
2019-07-25 16:28:43 -0400

    Fixed: missing arguments in test-coralwalk-noviz.sh

--- sunshine/scripts/test-coralwalk-noviz.sh
+++ sunshine/scripts/test-coralwalk-noviz.sh
@@ -3,8 +3,9 @@ if [ "$1" -eq "" ]; then
         echo "Error: you must supply a test image for test-coralwalk"
             exit 1
         fi
-roslaunch sunshine test-coralwalk-noviz.launch test_img:=$1 &
+roslaunch sunshine test-coralwalk-noviz.launch test_image:=$1 &
 rostopic echo -n 1 /image_walker/finished
-rosrun sunshine save_topic_map
-rosservice sunshine save_topics_scsv
+rosrun sunshine save_topic_map _output_prefix:=bw_topics
+rosrun sunshine save_topic_map _output_prefix:=color_topics _use_color:=true
+rosservice sunshine save_topics_by_cell_csv "filename: '$PWD/topics.csv'"
 kill ros
