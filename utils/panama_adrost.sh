a91e953ae3862b6df9b20aef14852dc122849270
Stewart Jamieson <sjamieson@whoi.edu>
2020-02-07 12:53:22 -0500

    Changed: panama.launch parameters to better match Doherty IROS'18

--- utils/panama_adrost.sh
+++ utils/panama_adrost.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-tmux new-session -d -s foo 'roslaunch sunshine panama.launch image_topic:=/bag18/camera/image_raw basename:=rost_1 || bash'
+tmux new-session -d -s foo 'roslaunch sunshine panama.launch image_topic:=/bag3/camera/image_raw basename:=rost_1 || bash'
 tmux rename-window 'panama_adrost'
 tmux select-window -t foo:0
-tmux split-window -h 'roslaunch sunshine panama.launch image_topic:=/bag19/camera/image_raw basename:=rost_2 || bash'
-tmux split-window -v -t 0 'rosbag play --clock /home/stewart/datasets/panama-bags/d20150418_18.bag /camera/image_raw:=/bag18/camera/image_raw --rate 3 || bash'
-tmux split-window -v -t 1 'ROS_NAMESPACE="bag19" rosbag play --clock /home/stewart/datasets/panama-bags/d20150419_19.bag /camera/image_raw:=/bag19/camera/image_raw --rate 3 || bash'
+tmux split-window -h 'roslaunch sunshine panama.launch image_topic:=/bag18/camera/image_raw basename:=rost_2 || bash'
+tmux split-window -v -t 0 'rosbag play /home/stewart/datasets/panama-bags/d20150418_18.bag /camera/image_raw:=/bag18/camera/image_raw --rate 4 || bash'
+tmux split-window -v -t 1 'rosbag play /home/stewart/datasets/panama-bags/d20150404_3.bag /camera/image_raw:=/bag3/camera/image_raw --rate 4 || bash'
 #tmux split-window -v -t 3 'rosrun sunshine model_translator _target_nodes:=rost_1,rost_2 _match_period:=5 _save_model_path:="/home/stewart/datasets/panama-bags/panama-topics" || bash'
 tmux split-window -v -t 3 "rosrun sunshine model_translator _target_nodes:=rost_1,rost_2 $* || bash"
 tmux -2 attach-session -t foo
