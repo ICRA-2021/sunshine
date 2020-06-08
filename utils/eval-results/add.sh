83201563bdea068a88e5c18c49d0e3857c708ae4
Stewart Jamieson <stewartjamieson@live.ca>
2020-06-07 21:29:46 -0400

    Fixed: folder for most recent sunshine_eval results

--- utils/eval-results/add.sh
+++ utils/eval-results/add.sh
@@ -1,2 +1,3 @@
-mkdir -p $(git log -n 1 --pretty=format:%H -- ../../sunshine)
-cd $(git log -n 1 --pretty=format:%H -- ../../sunshine)
+ID=$(git log -n 1 --date=short --pretty=format:%ad-%H -- ../../sunshine)
+mkdir -p $ID
+cd $ID
