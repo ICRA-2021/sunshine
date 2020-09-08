e7b3e860854f27d697384ce91c080514e81661bb
Stewart Jamieson <stewartjamieson@live.ca>
2020-09-08 01:51:22 -0400

    Fixed: bug where RobotSim would reject old tf messages

--- sunshine/nodes/multi_agent_sim.cpp
+++ sunshine/nodes/multi_agent_sim.cpp
@@ -99,6 +99,7 @@ class RobotSim {
         for (auto const &transform : tfMsg->transforms) {
             tf::StampedTransform tfTransform;
             tf::transformStampedMsgToTF(transform, tfTransform);
+            tfTransform.stamp_ = ros::Time::now();
             if (lastRgb && (tfTransform.frame_id_ == lastRgb->frame || tfTransform.child_frame_id_ == lastRgb->frame)) transform_found = true;
             wordTransformAdapter.addTransform(tfTransform);
             imageTransformAdapter.addTransform(tfTransform);
