e3bbb49f056c01b00126f651ba7cd0f679eca41d
Stewart Jamieson <stewartjamieson@live.ca>
2020-09-30 09:24:34 -0400

    Added: gamma and use_orb back into rost_optimize

--- sunshine/utilities/rost_optimize.cpp
+++ sunshine/utilities/rost_optimize.cpp
@@ -63,7 +63,7 @@ struct Eval {
   double const cell_space = 0.8;
 
   // number of input dimension (x.size())
-  BO_PARAM(size_t, dim_in, 3);
+  BO_PARAM(size_t, dim_in, 5);
   // number of dimensions of the result (res.size())
   BO_PARAM(size_t, dim_out, 1);
 
@@ -83,19 +83,19 @@ struct Eval {
 //      boost::math::lognormal space_dist(0.0, 1.0);
       double const alpha = boost::math::quantile(alpha_dist, x(0));
       double const beta = boost::math::quantile(beta_dist, x(1));
-      double const gamma = 0;
+      double const gamma = boost::math::quantile(gamma_dist, x(2));
 //      double const cell_space = .5 * std::pow(3., x(3));
-      bool const use_clahe = (x(2) >= 0.5);
+      bool const use_clahe = (x(3) >= 0.5);
 //      bool const use_texton = (x(5) >= 0.75);
-//      bool const use_orb = (x(6) >= 0.25);
+      bool const use_orb = (x(4) >= 0.25);
       sunshine::Parameters params{{{"alpha", alpha},
                                         {"beta", beta},
                                         {"gamma", gamma},
-                                        {"K", 10},
-                                        {"V", 180 /* * use_hue */ + 256 /* * use_color */ /* + 1000 * use_texton */ + 15000 /* * use_orb*/},
+                                        {"K", 20},
+                                        {"V", 180 /* * use_hue */ + 256 /* * use_color */ /* + 1000 * use_texton */ + 15000 * use_orb},
                                         {"use_clahe", use_clahe},
                                         {"use_texton", false},
-                                        {"use_orb", true},
+                                        {"use_orb", use_orb},
                                         {"cell_space", cell_space},
                                         {"cell_time", 3600.0},
                                         {"min_obs_refine_time", 300},
